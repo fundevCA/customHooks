2cecdbef1754c4c73f52de97d2f2f4ff569441b4
Blake Han <han.sangyeup@gmail.com>
2021-05-22 18:57:02 -0400

    use Network to trigger when the network goes offline

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # customHooks
 
 Project to create a Custom Hooks and Upload to NPM
+
+- [] useInput
+- [] useTabs
+- [] useTitle
+- [] useClick
+- [] useHover
+- [] useConfirm
+- [] usePreventLeave
+- [] useBeforeLeave
+- [] useFadeIn
+- [] useNetwork
+- [] useFullScreen
+- [] useNotification
+- [] useScroll
+- [] useAxios
---
+++ useNetwork/useNetwork.js
@@ -0,0 +1,19 @@
+export default useNetwork = onChange => {
+  const [status, setStatus] = useState(navigator.onLine);
+
+  const handleStatus = () => {
+    if (typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+  useEffect(() => {
+    document.addEventListener("online", handleStatus);
+    document.addEventListener("offline", handleStatus);
+    return () => {
+      document.removeEventListener("online", handleStatus);
+      document.removeEventListener("offline", handleStatus);
+    };
+  }, []);
+  return status;
+};
