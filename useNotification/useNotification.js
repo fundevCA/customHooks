e6b63da288c9b425a4dc51fd1ff44eb39b885f02
Blake Han <han.sangyeup@gmail.com>
2021-05-23 02:20:14 -0400

    use Notification to send a notification to user

---
+++ useNotification/useNotification.js
@@ -0,0 +1,22 @@
+export default useNotification = (title, message) => {
+  const options = {
+    body: message
+  };
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission === "granted") {
+      new Notification(title, options);
+    } else {
+      Notification.requestPermission().then(function(permission) {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    }
+  };
+  return fireNotif;
+};
