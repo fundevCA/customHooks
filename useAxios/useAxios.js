c78bb15db5ca006ed16a6e99257d39e6953dba31
Blake Han <han.sangyeup@gmail.com>
2021-05-23 14:24:40 -0400

    use Axios to fetch the data everytime user wants

---
+++ useAxios/useAxios.js
@@ -0,0 +1,29 @@
+export default useAxios = (httpReq, axiosInstance = axios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null
+  });
+  const [trigger, setTrigger] = useState(0);
+
+  // Everytime user calls the refetch, axios will make a new request
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true
+    });
+    setTrigger(new Date());
+  };
+
+  useEffect(() => {
+    try {
+      axiosInstance(httpReq).then(function(data) {
+        setState({ ...state, loading: false, data });
+      });
+    } catch (error) {
+      setState({ ...state, loading: false, error });
+    }
+  }, [trigger]);
+
+  return { ...state, refetch };
+};
