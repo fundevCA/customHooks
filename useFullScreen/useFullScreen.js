f553b092a38930ec9d29183d59417366892f4145
Blake Han <han.sangyeup@gmail.com>
2021-05-23 00:18:59 -0400

    use Full Screen to enter and exit full screen mode in different browser

---
+++ useFullScreen/useFullScreen.js
@@ -0,0 +1,38 @@
+export default useFullScreen = callback => {
+  const element = useRef();
+  const runCB = isFull => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
+  };
+
+  const enterFullScreen = () => {
+    if (element.current) {
+      element.current.requestFullscreen();
+    } else if (element.current.webkitRequestFullscreen) {
+      /* Safari */
+      element.current.webkitRequestFullscreen();
+    } else if (element.current.msRequestFullscreen) {
+      /* IE11 */
+      element.current.msRequestFullscreen();
+    } else if (element.current.mozRequestFullScreen) {
+      /* Mozilla */
+      element.current.mozRequestFullScreen();
+    }
+    runCB(true);
+  };
+  const exitFullScreen = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    runCB(false);
+  };
+
+  return { element, enterFullScreen, exitFullScreen };
+};
