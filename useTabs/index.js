75f2ef37462a744b2b167e7b3d4ad990167e9586
Blake Han <han.sangyeup@gmail.com>
2021-05-25 16:01:32 -0400

    Add Use Tabs Example and deploy the package to npm

--- useTabs/index.js
+++ useTabs/index.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 // Put initial index in first param and array in second param
 // This will return current Item and the new Item when Event is triggered
-export default useTabs = (initialTab, allTabs) => {
+export const useTabs = (initialTab, allTabs) => {
   const [currentIndex, setCurrentIndex] = useState(initialTab);
   if (!allTabs || !Array.isArray(allTabs)) return;
   return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
